53cfaa0f99af1c6ce2d2704c4fab135bf0c3ba85
Nova Tegar <nophategar@gmail.com>
2025-08-29 22:08:57 +0700

    fix: CRUD

--- app/layout.tsx
+++ app/layout.tsx
@@ -75,7 +75,7 @@ export const metadata = {
   },
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="id" className="scroll-smooth h-full">
       <head>
